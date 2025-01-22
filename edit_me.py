a517e89babfa6a21b617e111aff91be0bb95e3c7
MatteoPisu <mpisu@drury.edu>
2025-01-22 10:35:11 -0600

    lab 1

--- edit_me.py
+++ edit_me.py
@@ -5,8 +5,13 @@ def find_max(lst):
     current_max = lst[0]
 
     #
-    #  YOUR CODE GOES HERE
-    #
+    for i in range(1, len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i]
+    
+        
+
+    
 
     return current_max
 
